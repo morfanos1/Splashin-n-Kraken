bc987487751c89e180c423408ecf25bea280e616
Dietrich Epp <depp@zdome.net>
2017-01-21 11:24:58 -0500

    add project

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorScript : MonoBehaviour {
+
+    public float cursorSpeed;
+    public Boundary boundary;
+
+    public GameObject wavePrefab;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+
+    [System.Serializable]
+    public class Boundary
+    {
+        public float xMin, xMax, yMin, yMax;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * cursorSpeed;
+        float moveVertical = Input.GetAxis("Vertical") * Time.deltaTime * cursorSpeed;
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+
+        gameObject.transform.Translate(movement);
+
+        gameObject.transform.position = new Vector3
+        (
+            Mathf.Clamp(gameObject.transform.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(gameObject.transform.position.y, boundary.yMin, boundary.yMax),
+            0.0f
+        );
+
+        if(Input.GetButtonDown("Fire2"))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Instantiate(wavePrefab, gameObject.transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/WaveExpand.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveExpand : MonoBehaviour {
+
+    public ParticleSystem ps;
+    public CircleCollider2D cc;
+
+    public float startRadius;
+    public float endRadius;
+    public float expandSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(ExpandAndKill(startRadius, endRadius));
+    }
+
+    IEnumerator ExpandAndKill(float startRad, float endRad)
+    {
+        for(float i = startRad; i < endRad; i = i + expandSpeed*Time.deltaTime)
+        {
+            ParticleSystem.ShapeModule shapeModule = ps.shape;
+            shapeModule.radius = i;
+            //ps.shape.radius = i;
+            cc.radius = i;
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
