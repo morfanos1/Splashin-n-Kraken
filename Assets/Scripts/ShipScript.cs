fd53e735c28a8f882fc150cb080a9f88d18a30a3
Ryun Valdez <Ryun Valdez>
2017-01-22 10:18:10 -0500

    Added Ship Crash Animation

--- Assets/Scripts/ShipScript.cs
+++ Assets/Scripts/ShipScript.cs
@@ -7,7 +7,8 @@ public class ShipScript : MonoBehaviour
     public Rigidbody2D rb;
     public float maxVelocity;
 
-    public GameObject shipWreck;
+    //public GameObject shipWreck;
+    public Animator anim;
 
     // Use this for initialization
     void Start()
@@ -25,9 +26,11 @@ public class ShipScript : MonoBehaviour
     {
         if(collision.collider.tag == "Obstacle")
         {
-            Instantiate(shipWreck, gameObject.transform.position, Quaternion.identity);
-            Destroy(gameObject);       
+            //Instantiate(shipWreck, gameObject.transform.position, Quaternion.identity);
+            anim.SetTrigger("Crashed");
+            Destroy(gameObject, anim.GetCurrentAnimatorStateInfo(0).length);
         }
+        //Destroy(gameObject);       
     }
 
     void OnTriggerEnter2D(Collider2D other)
